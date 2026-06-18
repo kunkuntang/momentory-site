673db3cf109fb3be2fd66016064c10347c479fea
KuntangHuang <kuntang@163.com>
2026-06-18 23:19:47 +0800

    build：调整构建流程

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  base: '/beta',
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const baseMap = {
+    beta: '/beta',
+    prod: '/prod',
+  };
+
+  return {
+    base: baseMap[mode as keyof typeof baseMap] || '/beta',
+    plugins: [react()],
+  };
 });
