229a6a9d7b900e44e188a572d010cb68d86cc353
KuntangHuang <kuntang@163.com>
2026-08-16 09:01:02 +0800

    feat: 更新 prisma 配置获取db地址的方式

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig, env } from 'prisma/config';
 import 'dotenv/config';
 
+const DB_HOST = env('DB_HOST');
+const DB_PORT = env('DB_PORT');
+const DB_USER = env('DB_USER');
+const DB_PASSWORD = env('DB_PASSWORD');
+const DB_NAME = env('DB_NAME');
+const DATABASE_URL = `mysql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+
 export default defineConfig({
   schema: 'prisma/schema.prisma',
   datasource: {
-    url: env('DATABASE_URL'),
+    url: DATABASE_URL,
   },
 });
